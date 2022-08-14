3c28b48afa96b859d8975906b43c6c6e6207e797
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-08-15 02:31:57 +0900

    Update hokuyo3d.cpp

--- src/hokuyo3d.cpp
+++ src/hokuyo3d.cpp
@@ -1,1 +1,14 @@
+#include <rclcpp/rclcpp.hpp>
+#include <memory>
 
+#include "hokuyo3d/hokuyo3d_driver.hpp"
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  auto greeter = std::make_shared<greeter_ros2_style::Greeter>(options);
+  rclcpp::spin(greeter);
+
+  rclcpp::shutdown();
+  return 0;
+}
