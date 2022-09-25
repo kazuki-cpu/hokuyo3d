f01302d0a6bab445b37bc324eceb48a7608a4938
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-09-25 20:12:31 +0900

    Update hokuyo3d_driver.hpp

--- include/hokuyo3d_driver.hpp
+++ include/hokuyo3d_driver.hpp
@@ -40,15 +40,14 @@ extern "C" {
 #endif
 
 #include <rclcpp/rclcpp.hpp>
-#include <>
-#include <>
+
 
 namespace hokuyo3d
 {
 class Hokuyo3dNode : public rclcpp::Node
 {
 public:
-  HOKUYO3D_PUBLIC explicit HOKUYO3D(const rclcpp::NodeOptions & options);
+  HOKUYO3D_PUBLIC explicit Hokuyo3dNode(const rclcpp::NodeOptions & options);
   
   void cbPoint();
   void cbError();
@@ -61,11 +60,59 @@ public:
   void spin();
   void ping();
   
-private:
+
+protected:
+  
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud>SharedPtr pub_pc_; //更新9.17(9.2)
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>SharedPtr pub_pc2_; //更新9.17(9.2)
+  rclcpp::Publisher<sensor_msgs::msg::Imu>SharedPtr pub_imu_; //更新9.17(9.2)
+  rclcpp::Publisher<sensor_msgs::msg::MagneticField>SharedPtr pub_mag_; //更新9.17(9.2)
+  vssp::VsspDriver driver_;
+  sensor_msgs::msg::PointCloud cloud_; //変更9.17
+  sensor_msgs::msg::PointCloud2 cloud2_; //変更9.17
+  sensor_msgs::msg::Imu imu_; //変更9.17
+  sensor_msgs::msg::MagneticField mag_; //変更9.17
+
+  bool enable_pc_;
+  bool enable_pc2_;
+  bool allow_jump_back_;
+  std::mutex connect_mutex_;//変更9.17
+
+  rclcpp::Time time_ping_; //変更9.17
+  rclcpp::Time timestamp_base_; //変更9.17
+  std::deque<rclcpp::Time> timestamp_base_buffer_; //変更9.17
+  rclcpp::Time imu_stamp_last_; //変更9.17
+  rclcpp::Time mag_stamp_last_; //変更9.17
+  rclcpp::Time cloud_stamp_last_; //変更9.17
+
+  boost::asio::io_service io_;
+  boost::asio::deadline_timer timer_;
+
+  int field_;
+  int frame_;
+  int line_;
+
+  enum PublishCycle
+  {
+    CYCLE_FIELD,
+    CYCLE_FRAME,
+    CYCLE_LINE
+  };
+  PublishCycle cycle_;
+  std::string ip_;
+  int port_;
+  int horizontal_interlace_;
+  int vertical_interlace_;
+  double range_min_;
+  std::string frame_id_;
+  std::string imu_frame_id_;
+  std::string mag_frame_id_;
+  bool auto_reset_;
+  bool set_auto_reset_;
 
 
 };
   
-} //namespace Hokuyo3d
+} //namespace hokuyo3d
 
 #endif // HOKUYO3D__HOKUYO3D_DRIVER_HPP_
