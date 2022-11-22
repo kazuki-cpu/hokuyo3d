28e7834df36534d64ed4408f490209960339c1f0
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-11-23 04:20:10 +0900

    Update vssp.hpp

--- include/hokuyo3d/vssp.hpp
+++ include/hokuyo3d/vssp.hpp
@@ -1,34 +1,44 @@
-/*
- * Copyright (c) 2014, ATR, Atsushi Watanabe
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the copyright holder nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef HOKUYO3D__VSSP_HPP_
 #define HOKUYO3D__VSSP_HPP_
+
+#if __cplusplus
+extern "C" {
+#endif
+
+// The below macros are taken from https://gcc.gnu.org/wiki/Visibility and from
+// demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define HOKUYO3D_EXPORT __attribute__ ((dllexport))
+    #define HOKUYO3D_IMPORT __attribute__ ((dllimport))
+  #else
+    #define HOKUYO3D_EXPORT __declspec(dllexport)
+    #define HOKUYO3D_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef HOKUYO3D_BUILDING_DLL
+    #define HOKUYO3D_PUBLIC HOKUYO3D_EXPORT
+  #else
+    #define HOKUYO3D_PUBLIC HOKUYO3D_IMPORT
+  #endif
+  #define HOKUYO3D_PUBLIC_TYPE HOKUYO3D_PUBLIC
+  #define HOKUYO3D_LOCAL
+#else
+  #define HOKUYO3D_EXPORT __attribute__ ((visibility("default")))
+  #define HOKUYO3D_IMPORT
+  #if __GNUC__ >= 4
+    #define HOKUYO3D_PUBLIC __attribute__ ((visibility("default")))
+    #define HOKUYO3D_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define HOKUYO3D_PUBLIC
+    #define HOKUYO3D_LOCAL
+  #endif
+  #define HOKUYO3D_PUBLIC_TYPE
+#endif
+
+#if __cplusplus
+}  // extern "C"
+#endif
+
 
 #include <boost/asio.hpp>
 #include <boost/asio/system_timer.hpp>
