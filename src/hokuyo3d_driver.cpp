4c9ae404a0a43ecd18f98585cedd3c85076f1cfb
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-12-14 03:24:13 +0900

    Update hokuyo3d_driver.cpp

--- src/hokuyo3d_driver.cpp
+++ src/hokuyo3d_driver.cpp
@@ -45,8 +45,8 @@
 #include <hokuyo3d/hokuyo3d_driver.hpp>
 #include <hokuyo3d/vssp.hpp>
 
-using namespace std::placeholders
-using namespace std::chrono//12/2追加
+using namespace std::placeholders;
+using namespace std::chrono;//12/2追加
 
 namespace Hokuyo3d
 {
