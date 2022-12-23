13bb40c025cdc7ba2ea0bd7fe4ba73ab5bff2c5f
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-12-23 17:20:35 +0900

    Update hokuyo3d_driver.hpp

--- include/hokuyo3d/hokuyo3d_driver.hpp
+++ include/hokuyo3d/hokuyo3d_driver.hpp
@@ -71,8 +71,8 @@ public:
       const boost::shared_array<vssp::XYZI>& points);
   void cbError(const vssp::Header& header,
       const std::string& message);
-  //void cbPing(const vssp::Header& header,
-      //const std::chrono::system_clock::time_point& time_read);
+  void cbPing(const vssp::Header& header,
+      const std::chrono::system_clock::time_point& time_read);
   void cbAux(const vssp::Header& header,
       const vssp::AuxHeader& aux_header,
       const boost::shared_array<vssp::Aux>& auxs);
