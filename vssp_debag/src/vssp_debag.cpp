31c52a3f619f96b6e76eabaf7578aeead71ed575
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2022-12-29 19:48:42 +0900

    Update vssp_debag.cpp

--- vssp_debag/src/vssp_debag.cpp
+++ vssp_debag/src/vssp_debag.cpp
@@ -25,24 +25,24 @@ public:
 		int horizontal_interlace_ = 4;
 		int horizontal_interlace_ = 1;
 		std::string ip_ = "192.168.11.100";
-		int port_ = 10940;
-
-		boost::asio::ip::tcp::socket socket(io_);	
+		int port_ = 10940;	
 		
 		//publisher establish
 		header_pub = this->create_publisher<vssp_debag_msgs::msg::Header>("header", 10);
 		range_header_pub = this->create_publisher<vssp_debag_msgs::msg::RangeHeader>("range_header", 10);
 		aux_header_pub = this->create_publisher<vssp_debag_msgs::msg::AuxHeader>("aux_header", 10);
-		aux_pub = this->create_publisher<vssp_debag_msgs::msg::Aux>("aux", 10);
-		xyzi_pub = this->create_publisher<vssp_debag_msgs::msg::XYZI>("xyzi", 10);
+		//aux_pub = this->create_publisher<vssp_debag_msgs::msg::Aux>("aux", 10);
+		//xyzi_pub = this->create_publisher<vssp_debag_msgs::msg::XYZI>("xyzi", 10);
 		
 		//register callback function
 		driver_.registerHeaderCallback(std::bind(&YVTcommunication::Header_publish, this, _1));
 		driver_.registerRangeHeaderCallback(std::bind(&YVTcommunication::RangeHeader_publish, this, _1));
 		driver_.registerAuxHeaderCallback(std::bind(&YVTcommunication::AuxHeader_publish, this, _1));
-		driver_.registerXYZICallback(std::bind(&YVTcommunication::XYZI_publish, this, _1));
-		driver_.registerAuxCallback(std::bind(&YVTcommunication::Aux_publish, this, _1));
+		//driver_.registerXYZICallback(std::bind(&YVTcommunication::XYZI_publish, this, _1));
+		//driver_.registerAuxCallback(std::bind(&YVTcommunication::Aux_publish, this, _1));
   
+  		std::lock_guard<std::mutex> lock(connect_mutex_);
+		
   		tcp_ip_connect(ip_, port_);
 		spin();
 	}
@@ -63,7 +63,6 @@ public:
     		timer_.async_wait(std::bind(&VsspDriver::onTimeoutConnect, driver_, boost::asio::placeholders::error));
     		socket_.async_connect(endpoint, std::bind(&YVTcommunication::vssp_connect, this, boost::asio::placeholders::error));
 	}
-	
 	void vssp_connect()
 	{
 		timer_.cancel();
@@ -77,7 +76,6 @@ public:
       		driver_.requestAuxData();
 		driver_.receivePackets();		
      	}
-	
 	void cbTimer(const boost::system::error_code& error)
   	{
     		if (error)
@@ -108,54 +106,45 @@ public:
   	}
 	void Header_publish(const vssp::Header& header)
   	{
-    		header_->mark = header.mark;
-    		header_->type = header.type;
-    		header_->status = header.status;
-    		header_->header_length = header.header_length;
-    		header_->length = header.length;
-    		header_->received_time_ms = header.received_time_ms;
-    		header_->send_time_ms = header.send_time_ms;
+    		header_.mark = header.mark;
+    		header_.type = header.type;
+    		header_.status = header.status;
+    		header_.header_length = header.header_length;
+    		header_.length = header.length;
+    		header_.received_time_ms = header.received_time_ms;
+    		header_.send_time_ms = header.send_time_ms;
     		header_pub->publish(header_);
   	}
   	void RangeHeader_publish(const vssp::RangeHeader& range_header, 
 				 const vssp::RangeHeaderV2R1& range_header_v2r1)
   	{
-    		range_header_->header_length = range_header.header_length;
-    		range_header_->line_head_timestamp_ms = range_header.line_head_timestamp_ms;
-    		range_header_->line_tail_timestamp_ms = range_header.line_tail_timestamp_ms;
-    		range_header_->line_head_h_angle_ratio = range_header.line_head_h_angle_ratio;
-    		range_header_->line_tail_h_angle_ratio = range_header.line_tail_h_angle_ratio;
-    		range_header_->frame = range_header.frame;
-    		range_header_->field = range_header.field;
-    		range_header_->line = range_header.line;
-    		range_header_->spot = range_header.spot;
-    		range_header_->vertical_field = range_header_v2r1.vertical_field;
-    		range_header_->vertical_interlace = range_header_v2r1.vertical_interlace;
+    		range_header_.header_length = range_header.header_length;
+    		range_header_.line_head_timestamp_ms = range_header.line_head_timestamp_ms;
+    		range_header_.line_tail_timestamp_ms = range_header.line_tail_timestamp_ms;
+    		range_header_.line_head_h_angle_ratio = range_header.line_head_h_angle_ratio;
+    		range_header_.line_tail_h_angle_ratio = range_header.line_tail_h_angle_ratio;
+    		range_header_.frame = range_header.frame;
+    		range_header_.field = range_header.field;
+    		range_header_.line = range_header.line;
+    		range_header_.spot = range_header.spot;
+    		range_header_.vertical_field = range_header_v2r1.vertical_field;
+    		range_header_.vertical_interlace = range_header_v2r1.vertical_interlace;
     		range_header_pub->publish(range_header_);
   	}
   	void AuxHeader_publish(const vssp::AuxHeader& aux_header)
   	{
-    		aux_header_->header_length = aux_header.header_length;
-    		aux_header_->timestamp_ms = aux_header.timestamp_ms;
-    		aux_header_->data_bitfield = aux_header.data_bitfield;
-    		aux_header_->data_count = aux_header.data_count;
-    		aux_header_->data_ms = aux_header.data_ms;
+    		aux_header_.header_length = aux_header.header_length;
+    		aux_header_.timestamp_ms = aux_header.timestamp_ms;
+    		aux_header_.data_bitfield = aux_header.data_bitfield;
+    		aux_header_.data_count = aux_header.data_count;
+    		aux_header_.data_ms = aux_header.data_ms;
     		aux_header_pub->publish(aux_header_);
-  	}
-  	void XYZI_publish(const boost::shared_array<vssp::XYZI>& points)
-  	{
-    
-  	}
-  	void Aux_publish(const boost::shared_array<vssp::Aux>& auxs)
-  	{
-    
   	}
 
 protected:
 
-   boost::asio::io_service io_;
-   boost::asio::system_timer timer_;
    vssp::VsspDriver driver_;
+   std::mutex connect_mutex_;
    
    vssp_debag_msgs::msg::Header header_;
    vssp_debag_msgs::msg::RangeHeader range_header_;
