92301988432340e237c0e715aa2aee007a972c3b
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2023-01-07 12:39:49 +0900

    Update hokuyo3d_driver.cpp

--- src/hokuyo3d_driver.cpp
+++ src/hokuyo3d_driver.cpp
@@ -67,8 +67,7 @@ namespace Hokuyo3d
     allow_jump_back_ = this->declare_parameter<bool>("allow_jump_back", false);
 
     std::string output_cycle;
-    output_cycle = this->declare_parameter<std::string>("output_cycle", "field");
-        
+    output_cycle = this->declare_parameter<std::string>("output_cycle", "field");    
     if (output_cycle.compare("frame") == 0)
       cycle_ = CYCLE_FRAME;
     else if (output_cycle.compare("field") == 0)
@@ -103,11 +102,10 @@ namespace Hokuyo3d
                                       sensor_msgs::msg::PointField::FLOAT32, "z", 1, sensor_msgs::msg::PointField::FLOAT32,
                                       "intensity", 1, sensor_msgs::msg::PointField::FLOAT32);    
 
-    pub_imu_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", 5);
-
     std::lock_guard<std::mutex> lock(connect_mutex_);
     pub_pc_ = this->create_publisher<sensor_msgs::msg::PointCloud>("hokuyo_cloud", 5);
     pub_pc2_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("hokuyo_cloud2", 5);
+    pub_imu_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", 5);
 
     // Start communication with the sensor
     driver_.connect(ip_.c_str(), port_, std::bind(&Hokuyo3dNode::cbConnect, this, _1));
@@ -123,34 +121,11 @@ namespace Hokuyo3d
     RCLCPP_INFO(this->get_logger(), "Communication stoped");
   }
   
-  void Hokuyo3dNode::cbConnect(bool success)
-  {
-    if (success)
-    {
-      RCLCPP_INFO(get_logger(), "Connection established");
-      ping();
-      if (set_auto_reset_)
-        driver_.setAutoReset(auto_reset_);
-      driver_.setHorizontalInterlace(horizontal_interlace_);
-      driver_.requestHorizontalTable();
-      driver_.setVerticalInterlace(vertical_interlace_);
-      driver_.requestVerticalTable(vertical_interlace_);
-      driver_.requestData(true, true);
-      driver_.requestAuxData();
-      driver_.receivePackets();
-      RCLCPP_INFO(get_logger(), "Communication started");
-    }
-    else
-    {
-      RCLCPP_ERROR(get_logger(), "Connection failed");
-    }
-  }
-  
   void Hokuyo3dNode::cbPoint(
       const vssp::RangeHeader& range_header,
       const vssp::RangeIndex& range_index,
       const boost::shared_array<uint16_t>& index,
-      const boost::shared_array<vssp::XYZI>& points)//12/2変更
+      const boost::shared_array<vssp::XYZI>& points)
   {
     //if (timestamp_base_ == rclcpp::Time(0, 0))
       //return;
@@ -250,11 +225,6 @@ namespace Hokuyo3d
       line_ = range_header.line;
     }
   }
-  void Hokuyo3dNode::cbError(
-      const std::string& message)
-  {
-    RCLCPP_ERROR(get_logger(), "%s", message.c_str());
-  }
   
   void Hokuyo3dNode::cbAux(
       const vssp::AuxHeader& aux_header,
@@ -292,6 +262,47 @@ namespace Hokuyo3d
       }
     }
   }
+  
+  void Hokuyo3dNode::cbError(
+      const std::string& message)
+  {
+    RCLCPP_ERROR(get_logger(), "%s", message.c_str());
+  }
+  
+  void Hokuyo3dNode::cbConnect(bool success)
+  {
+    if (success)
+    {
+      RCLCPP_INFO(get_logger(), "Connection established");
+      ping();
+      if (set_auto_reset_)
+        driver_.setAutoReset(auto_reset_);
+      driver_.setHorizontalInterlace(horizontal_interlace_);
+      driver_.requestHorizontalTable();
+      driver_.setVerticalInterlace(vertical_interlace_);
+      driver_.requestVerticalTable(vertical_interlace_);
+      driver_.requestData(true, true);
+      driver_.requestAuxData();
+      driver_.receivePackets();
+      RCLCPP_INFO(get_logger(), "Communication started");
+    }
+    else
+    {
+      RCLCPP_ERROR(get_logger(), "Connection failed");
+    }
+  }
+  
+  void Hokuyo3dNode::spin()
+  {
+    timer_.async_wait(std::bind(&Hokuyo3dNode::cbTimer, this, _1));
+    std::thread thread(
+        std::bind(
+  static_cast<std::size_t (boost::asio::io_service::*)()>(&boost::asio::io_service::run), &io_));//12/2変更
+
+    driver_.spin();
+    timer_.cancel();
+    RCLCPP_INFO(get_logger(),"Connection closed");
+  }
 
   bool Hokuyo3dNode::poll()
   {
@@ -302,6 +313,7 @@ namespace Hokuyo3d
     RCLCPP_INFO(get_logger(), "Connection closed");
     return false;
   }
+  
   void Hokuyo3dNode::cbTimer(const boost::system::error_code& error)
   {
     if (error)
@@ -319,35 +331,25 @@ namespace Hokuyo3d
       timer_.async_wait(std::bind(&Hokuyo3dNode::cbTimer, this, _1));
     }
   }
-  void Hokuyo3dNode::spin()
-  {
-    timer_.async_wait(std::bind(&Hokuyo3dNode::cbTimer, this, _1));
-    std::thread thread(
-        std::bind(
-  static_cast<std::size_t (boost::asio::io_service::*)()>(&boost::asio::io_service::run), &io_));//12/2変更
 
-    driver_.spin();
-    timer_.cancel();
-    RCLCPP_INFO(get_logger(),"Connection closed");
-  }
   void Hokuyo3dNode::ping()
   {
     driver_.requestPing();
     //time_ping_ = this->now();
   }
+  
   /*void Hokuyo3dNode::cbPing(
       const vssp::Header& header,
       const system_clock::time_point& time_read)
   {
     
-    milliseconds now = duration_cast<milliseconds>(time_read.time_since_epoch());//12/12変更
+    milliseconds now = duration_cast<milliseconds>(time_read.time_since_epoch());
     builtin_interfaces::msg::Time ping_time = time_ping_;
 
     const rclcpp::Duration delay =
         (rclcpp::Duration(now) - rclcpp::Duration(ping_time.sec, ping_time.nanosec) - rclcpp::Duration(header.send_time_ms * 0.001 - header.received_time_ms * 0.001)) * 0.5;
     const timestamp_base_ = time_ping_ + delay - rclcpp::Duration(header.received_time_ms * 0.001);
-  */ 
-    /*
+  
     timestamp_base_buffer_.push_back(base);
     if (timestamp_base_buffer_.size() > 5)
       timestamp_base_buffer_.pop_front();
@@ -355,16 +357,16 @@ namespace Hokuyo3d
     auto sorted_timstamp_base = timestamp_base_buffer_;
     std::sort(sorted_timstamp_base.begin(), sorted_timstamp_base.end());
     
-    if (timestamp_base_ == rclcpp::Time(0,0)){//12/12変更
+    if (timestamp_base_ == rclcpp::Time(0,0)){
       timestamp_base_ = sorted_timstamp_base[sorted_timstamp_base.size() / 2];
     }else{
       builtin_interfaces::msg::Time old_timestamp_base = timestamp_base_;
       builtin_interfaces::msg::Time new_timestamp_base = sorted_timstamp_base[sorted_timstamp_base.size() / 2];
       timestamp_base_ = timestamp_base_ + rclcpp::Duration(new_timestamp_base.sec - old_timestamp_base.sec, new_timestamp_base.nanosec - old_timestamp_base.nanosec)* 0.1;
     }
-    */
-    /*RCLCPP_DEBUG(get_logger(), "timestamp_base: %lf", timestamp_base_.seconds());//12/3変更
+    RCLCPP_DEBUG(get_logger(), "timestamp_base: %lf", timestamp_base_.seconds());
   }*/
+  
 }
 
 #include "rclcpp_components/register_node_macro.hpp"
