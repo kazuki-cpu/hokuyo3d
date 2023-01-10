cbdfa007b4daf8b40e131b8b0600fad88cd2ef7b
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2023-01-10 17:16:59 +0900

    Update hokuyo3d_driver.hpp

--- include/hokuyo3d/hokuyo3d_driver.hpp
+++ include/hokuyo3d/hokuyo3d_driver.hpp
@@ -94,7 +94,7 @@ protected:
   builtin_interfaces::msg::Time cloud_stamp_last_;
   builtin_interfaces::msg::Time cloud2_stamp_last_; 
   builtin_interfaces::msg::Time imu_stamp_last_; 
-  rclcpp::Time time_ping_; 
+  std::chrono::system_clock::time_point time_ping_; 
   rclcpp::Time timestamp_base_; 
   //std::deque<rclcpp::Time> timestamp_base_buffer_; 
 
